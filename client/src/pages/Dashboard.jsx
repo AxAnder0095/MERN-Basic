6c862bda74580d7dca96ac7f9655f4ef85720646
AxAnder0095 <doriscafe@hotmail.com>
2026-02-15 11:52:53 -0600

    minimal dashboard styling

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,30 +1,40 @@
 import { Logout } from "../components/Logout.jsx";
 import { useItems } from "../hooks/useItems.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
+import "../styles/Dashboard.scss";
 
 export const Dashboard = () => {
   const { items, postItem } = useItems();
   const { user } = useAuth0();
 
+  if (!items) {
+    return <p>Loading...</p>;
+  }
+
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <h3>Welcome {user?.name}!</h3>
-      <button onClick={postItem}>Add Item</button>
+    <div className="dashboard">
+      <div className="dashboard-content">
+        <h1>Dashboard</h1>
+        <h3>Welcome {user?.name}!</h3>
+        <button onClick={postItem}>Add Item</button>
 
-      {items.length > 0 ? (
-        <ul>
-          {items.map((item) => (
-            <li key={item._id}>
-              Working: {item.working ? "true" : "false"}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <span>No items found.</span>
-      )}
-      <Logout />
+        <Logout />
+      </div>
     </div>
   );
 }
+
+
+
+// {items.length > 0 ? (
+//         <ul>
+//           {items.map((item) => (
+//             <li key={item._id}>
+//               Working: {item.working ? "true" : "false"}
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <span>No items found.</span>
+//       )}
